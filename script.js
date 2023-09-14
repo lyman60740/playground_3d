aaf261fef7ccd418c2d2b499c98aceccf824481f
Lyman Abid <merge.tool>
2023-09-14 21:30:51 +0200

    m

--- script.js
+++ script.js
@@ -1,105 +1,135 @@
-
-// Création de la scène
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  20,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 35;
+camera.position.y = 1;
+camera.position.x = 1;
+camera.position.x += -20; // Décale la caméra de 5 unités sur l'axe X
+camera.position.y += 20;
 
-// Création de la caméra
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 20;
-camera.position.y = 4;
-camera.position.x = 4;
-camera.lookAt(new THREE.Vector3(1, 4, 4));
+const moveAmountX = 10;
+camera.position.x -= moveAmountX;
+const moveAmountY = -5;
+camera.position.y -= moveAmountY;
+camera.lookAt(new THREE.Vector3(1 - moveAmountX, 1 - moveAmountY, 4));
 
-
-
-// Création du renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xf2f0ed);
 document.body.appendChild(renderer.domElement);
 
-const cubeSpacing = 2; // Espace entre les cubes
+const animationProps = { cubeSpacing: 1, cubeRotation: 0 };
+const cubesAndLines = [];
 
-// Partie verticale
-for (let i = 0; i < 3; i++) { // Largeur
-    for (let j = 0; j < 8; j++) { // Hauteur
-        for (let l = 0; l < 9; l++) { // Profondeur
-            if (l > 0 && j == 0) continue; // Cette condition permet d'éviter les cubes du bas, puisque la partie horizontale les remplira
+function createOrMoveCubes() {
+  let index = 0;
 
-            // Géométrie et matériaux
-            const geometry = new THREE.BoxGeometry(1, 1, 1);
-            const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
-            const edges = new THREE.EdgesGeometry(geometry);
-            const edgesMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 6; j++) {
+      for (let l = 0; l < 7; l++) {
+        if (l > 0 && j == 0) continue;
 
-            // Création des cubes et de leurs bordures
-            const cube = new THREE.Mesh(geometry, cubeMaterial);
-            const line = new THREE.LineSegments(edges, edgesMaterial);
+        let cube, line;
 
-            // Positionnement des cubes
-            cube.position.x = i * cubeSpacing;
-            cube.position.y = j * cubeSpacing;
-            cube.position.z = l * cubeSpacing;
+        if (cubesAndLines[index]) {
+          [cube, line] = cubesAndLines[index];
+        } else {
+          const geometry = new THREE.BoxGeometry(1, 1, 1);
+          const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x171614 });
+          const edges = new THREE.EdgesGeometry(geometry);
+          const edgesMaterial = new THREE.LineBasicMaterial({
+            color: 0xf2f0ed,
+          });
 
-            line.position.x = i * cubeSpacing;
-            line.position.y = j * cubeSpacing;
-            line.position.z = l * cubeSpacing;
+          cube = new THREE.Mesh(geometry, cubeMaterial);
+          line = new THREE.LineSegments(edges, edgesMaterial);
 
-            // Ajout à la scène
-            scene.add(cube);
-            scene.add(line);
+          cubesAndLines.push([cube, line]);
+          scene.add(cube);
+          scene.add(line);
         }
+
+        cube.position.x = i * animationProps.cubeSpacing;
+        cube.position.y = j * animationProps.cubeSpacing;
+        cube.position.z = l * animationProps.cubeSpacing;
+        cube.rotation.y = animationProps.cubeRotation;
+
+        line.position.copy(cube.position);
+        line.rotation.y = animationProps.cubeRotation;
+
+        index++;
+      }
     }
-}
+  }
 
-// Partie horizontale
-for (let i = 0; i < 3; i++) { // Largeur
-    for (let k = 1; k <= 8; k++) { // Profondeur
+  for (let i = 0; i < 3; i++) {
+    for (let k = 1; k <= 6; k++) {
+      let cube, line;
 
-        // Géométrie et matériaux
+      if (cubesAndLines[index]) {
+        [cube, line] = cubesAndLines[index];
+      } else {
         const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+        const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x171614 });
         const edges = new THREE.EdgesGeometry(geometry);
-        const edgesMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+        const edgesMaterial = new THREE.LineBasicMaterial({ color: 0xf2f0ed });
 
-        // Création des cubes et de leurs bordures
-        const cube = new THREE.Mesh(geometry, cubeMaterial);
-        const line = new THREE.LineSegments(edges, edgesMaterial);
+        cube = new THREE.Mesh(geometry, cubeMaterial);
+        line = new THREE.LineSegments(edges, edgesMaterial);
 
-        // Positionnement des cubes
-        cube.position.x = i * cubeSpacing;
-        cube.position.z = k * cubeSpacing;
-
-        line.position.x = i * cubeSpacing;
-        line.position.z = k * cubeSpacing;
-
-        // Ajout à la scène
+        cubesAndLines.push([cube, line]);
         scene.add(cube);
         scene.add(line);
+      }
+
+      cube.position.x = i * animationProps.cubeSpacing;
+      cube.position.z = k * animationProps.cubeSpacing;
+      cube.rotation.y = animationProps.cubeRotation;
+      line.position.copy(cube.position);
+      line.rotation.y = animationProps.cubeRotation;
+      index++;
     }
+  }
 }
 
+createOrMoveCubes();
 
-
-
-
-
-// Animation
 function animate() {
-    requestAnimationFrame(animate);
-
-    scene.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
-// Ajustement de la taille du rendu lors du redimensionnement de la fenêtre
-window.addEventListener('resize', function() {
-    const newWidth = window.innerWidth;
-    const newHeight = window.innerHeight;
+window.addEventListener("resize", function () {
+  const newWidth = window.innerWidth;
+  const newHeight = window.innerHeight;
 
-    camera.aspect = newWidth / newHeight;
-    camera.updateProjectionMatrix();
+  camera.aspect = newWidth / newHeight;
+  camera.updateProjectionMatrix();
 
-    renderer.setSize(newWidth, newHeight);
+  renderer.setSize(newWidth, newHeight);
+});
+
+document.querySelector("body").addEventListener("click", () => {
+  gsap.to(animationProps, {
+    cubeSpacing: 5,
+    cubeRotation: Math.PI,
+    duration: 5,
+    onUpdate: createOrMoveCubes,
+    ease: "power1.inOut",
+    onComplete: function () {
+      gsap.to(animationProps, {
+        cubeSpacing: 1,
+        cubeRotation: 0,
+        duration: 5,
+        onUpdate: createOrMoveCubes,
+        ease: "power1.inOut",
+      });
+    },
+  });
 });
 
 animate();
